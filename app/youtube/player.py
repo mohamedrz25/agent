81f40525ad06a17dd5ff3c738ef2c99b70c0c4cd
mohamedrz25 <mohamed3211r@gmail.com>
2026-09-08 14:32:00 +0530

    Update player.py

--- app/youtube/player.py
+++ app/youtube/player.py
@@ -56,5 +56,20 @@ def create_youtube_url(command):
         text
     )
 
+    if match:
 
+        query = match.group(1)
+        break
+query = query.strip()
+
+video_id = get_vid(query)
+
+if not video_id:
+    return None
+
+return (
+    "https://www.youtube.com/embed/"
+    + video_id
+    + "?autoplay=1&mute=0"
+)
   
